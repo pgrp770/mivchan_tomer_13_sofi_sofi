fe23952dc17190bbe4a0ce9cb3770caed552ad03
pgrp770 <pgrp770@gmail.com>
2024-11-28 16:41:27 +0200

    code style: improve code style of all endpoints

--- app/route/phone_tracker_route.py
+++ app/route/phone_tracker_route.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from app.repository.call_repository import get_all_bluetooth_connection, \
-    get_all_devices_with_signal_stronger_than_60, get_connected_devices_by_id, get_latest_timestamp_relation
+    get_all_devices_with_signal_stronger_than_60, get_latest_timestamp_relation
 from app.service.service_phone_route import is_there_connection, insert_phone_call_to_neo4j, \
     get_amount_of_connected_devices
 
@@ -11,7 +11,7 @@ phone_blueprint = Blueprint("phone_tracker", __name__)
 def get_interaction():
     phone_call = request.json
     insert_phone_call_to_neo4j(phone_call)
-    return jsonify({}), 200
+    return jsonify({"message: the phone_call was inserted to neo4j"}), 200
 
 
 @phone_blueprint.route("/bluetooth_connection", methods=["GET"])
@@ -35,12 +35,14 @@ def get_amount_connected_devices(device_id):
 @phone_blueprint.route("/check_connection/<string:device_id_1>/<string:device_id_2>", methods=["GET"])
 def check_connection(device_id_1: str, device_id_2: str):
     result = is_there_connection(device_id_1, device_id_2)
-    return jsonify(result), 200
+    return jsonify({"there_is_connection": result}), 200
 
 
 @phone_blueprint.route("/latest_call/<string:device_id_1>/<string:device_id_2>", methods=["GET"])
 def latest_call(device_id_1: str, device_id_2: str):
     result = get_latest_timestamp_relation(device_id_1, device_id_2)
+
     if not result:
         return jsonify(({"message": "there is not calls between those devices"})), 200
+
     return jsonify({"latest_call": result}), 200
